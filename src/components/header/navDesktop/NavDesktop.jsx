bcb13ec79649f27fa0e1b225adcc771ab5cdabe7
Hossein <hosseinh1075@gmail.com>
2025-10-10 11:03:12 +0330

    add sub menu and complete nav

--- src/components/header/navDesktop/NavDesktop.jsx
+++ src/components/header/navDesktop/NavDesktop.jsx
@@ -5,62 +5,66 @@ import { PiShoppingCartLight } from "react-icons/pi";
 function NavDesktop() {
     return (
         <>
-            <nav className='bg-orange-200 lg:block hidden'>
+            <nav className='fixed top-0 left-0 right-0 bg-orange-200 lg:block hidden'>
                 <div className="container">
                     <div className='flex items-center justify-between p-6.25 xl:mx-15'>
                         <a href="#">
                             <img src="/icons/logo.png" alt="e-bazar" />
                         </a>
 
-                        <ul className='flexCenter gap-8 *:hover:text-orange-600 *:transition-all *:duration-500 *:ease-in-out'>
+                        <ul className='flexCenter gap-8'>
                             <li className='text-orange-600 transition-all duration-500 ease-in-out'>
-                                <a href="#" className='text-xl'>
+                                <a href="#" className='text-xl hover:text-orange-600 transition-all duration-500 ease-in-out'>
                                     Home
                                 </a>
                             </li>
                             <li>
-                                <a href="#" className='text-xl'>
+                                <a href="#" className='text-xl hover:text-orange-600 transition-all duration-500 ease-in-out'>
                                     Shop
                                 </a>
                             </li>
                             <li>
-                                <a href="#" className='text-xl'>
+                                <a href="#" className='text-xl hover:text-orange-600 transition-all duration-500 ease-in-out'>
                                     About
                                 </a>
                             </li>
-                            <li>
-                                <a href="#" className='text-xl'>
+                            <li className='relative group'>
+                                <a href="#" className='text-xl hover:text-orange-600 transition-all duration-500 ease-in-out'>
                                     Blogs
                                 </a>
 
                                 {/* subMenu */}
 
-                                <ul className='hidden'>
+                               <div className='absolute top-[105%] hidden group-hover:block bg-orange-50 border-t-3 border-solid border-orange-600 rounded-t-none rounded-b-[25px] py-3.75 px-6 z-40 w-60 shadow-sub'>
+                                 <ul className='*:py-2'>
                                     <li>
-                                        <a href="#">Blog Listing</a>
+                                        <a href="#" className='text-xl block hover:text-orange-600 transition-all duration-500 ease-in-out'>Blog Listing</a>
                                     </li>
                                     <li>
-                                        <a href="#">Blog Detail</a>
+                                        <a href="#" className='text-xl block hover:text-orange-600 transition-all duration-500 ease-in-out'>Blog Detail</a>
                                     </li>
                                 </ul>
+                               </div>
                             </li>
-                            <li>
-                                <a href="#" className='leading-[140%] text-xl'>
+                            <li className='relative group'>
+                                <a href="#" className='text-xl hover:text-orange-600 transition-all duration-500 ease-in-out'>
                                     Pages
                                 </a>
                                   {/* subMenu */}
 
-                                <ul className='hidden'>
+                              <div className='absolute top-[105%] hidden group-hover:block bg-orange-50 border-t-3 border-solid border-orange-600 rounded-t-none rounded-b-[25px] py-3.75 px-6 z-40 w-60 shadow-sub'>
+                                  <ul className='*:py-2'>
                                     <li>
-                                        <a href="#">Product Detail</a>
+                                        <a href="#" className='text-xl block hover:text-orange-600 transition-all duration-500 ease-in-out'>Product Detail</a>
                                     </li>
                                     <li>
-                                        <a href="#">Cart</a>
+                                        <a href="#" className='text-xl block hover:text-orange-600 transition-all duration-500 ease-in-out'>Cart</a>
                                     </li>
                                     <li>
-                                        <a href="#">Checkout</a>
+                                        <a href="#" className='text-xl block hover:text-orange-600 transition-all duration-500 ease-in-out'>Checkout</a>
                                     </li>
                                 </ul>
+                              </div>
                             </li>
                         </ul>
 
--- src/components/header/navMobile/NavMobile.jsx
+++ src/components/header/navMobile/NavMobile.jsx
@@ -10,71 +10,75 @@ function NavMobile() {
     }
     return (
         <>
-            <nav className='bg-orange-200 block lg:hidden relative'>
+            <nav className='fixed top-0 left-0 right-0 bg-orange-200 block lg:hidden relative'>
                 <div className="container">
                     <div className='flex items-center justify-between p-6.25'>
                         <a href="#">
                             <img src="/icons/logo.png" alt="e-bazar" />
                         </a>
 
-                        <a href="#" className='inline-block *:size-5' onClick={openSubMenu}>
-                            {open ? <IoMdClose /> : <FaBars />}
+                        <a href="#" className='inline-block *:size-[25px]' onClick={openSubMenu}>
+                            {open ? <IoMdClose/> : <FaBars />}
 
                         </a>
 
                         {/* subMenu */}
 
                         <div className={`absolute top-[67%] right-[12%] bg-orange-50 border-t-3 border-solid border-orange-600 rounded-t-none rounded-b-[25px] py-3.75 px-6 z-40 w-50 shadow-sub 
-                            overflow-hidden transition-all duration-500 ease-in-out ${open ? 'max-w-63 opacity-100' : 'max-w-0 opacity-0'}`}>
-                            <ul className='*:hover:text-orange-600 *:transition-all *:duration-500 *:ease-in-out *:py-2'>
+                             transition-all duration-500 ease-in-out ${open ? 'max-w-63 opacity-100' : 'max-w-0 opacity-0'}`}>
+                            <ul className='*:py-2'>
                                 <li className='text-orange-600 transition-all duration-500 ease-in-out'>
-                                    <a href="#" className='text-xl'>
+                                    <a href="#" className='text-xl hover:text-orange-600 transition-all duration-500 ease-in-out'>
                                         Home
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="#" className='text-xl'>
+                                    <a href="#" className='text-xl hover:text-orange-600 transition-all duration-500 ease-in-out'>
                                         Shop
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="#" className='text-xl'>
+                                    <a href="#" className='text-xl hover:text-orange-600 transition-all duration-500 ease-in-out'>
                                         About
                                     </a>
                                 </li>
-                                <li>
-                                    <a href="#" className='text-xl'>
+                                <li className='relative group'>
+                                    <a href="#" className='text-xl hover:text-orange-600 transition-all duration-500 ease-in-out'>
                                         Blogs
                                     </a>
 
                                     {/* subMenu */}
 
-                                    <ul className='hidden'>
-                                        <li>
-                                            <a href="#">Blog Listing</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Blog Detail</a>
-                                        </li>
-                                    </ul>
+                                    <div className='absolute top-[96%] right-[64%] hidden group-hover:block z-50 bg-orange-50 border-t-3 border-solid border-orange-600 rounded-t-none rounded-b-[25px] py-3.75 px-6 z-40 w-60 shadow-sub'>
+                                        <ul className='*:py-2'>
+                                            <li>
+                                                <a href="#" className='text-xl block hover:text-orange-600 transition-all duration-500 ease-in-out'>Blog Listing</a>
+                                            </li>
+                                            <li>
+                                                <a href="#" className='text-xl block hover:text-orange-600 transition-all duration-500 ease-in-out'>Blog Detail</a>
+                                            </li>
+                                        </ul>
+                                    </div>
                                 </li>
-                                <li>
-                                    <a href="#" className='text-xl'>
+                                <li className='relative group'>
+                                    <a href="#" className='text-xl hover:text-orange-600 transition-all duration-500 ease-in-out'>
                                         Pages
                                     </a>
                                     {/* subMenu */}
 
-                                    <ul className='hidden'>
-                                        <li>
-                                            <a href="#">Product Detail</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Cart</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Checkout</a>
-                                        </li>
-                                    </ul>
+                                     <div className='absolute top-[96%] right-[64%] hidden group-hover:block bg-orange-50 border-t-3 border-solid border-orange-600 rounded-t-none rounded-b-[25px] py-3.75 px-6 z-40 w-60 shadow-sub'>
+                                  <ul className='*:py-2'>
+                                    <li>
+                                        <a href="#" className='text-xl block hover:text-orange-600 transition-all duration-500 ease-in-out'>Product Detail</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className='text-xl block hover:text-orange-600 transition-all duration-500 ease-in-out'>Cart</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className='text-xl block hover:text-orange-600 transition-all duration-500 ease-in-out'>Checkout</a>
+                                    </li>
+                                </ul>
+                              </div>
                                 </li>
                             </ul>
                         </div>
