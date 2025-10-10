ae9f7aa3434c9a56554caac699704557a0cfc51a
Hossein <hosseinh1075@gmail.com>
2025-10-10 12:47:37 +0330

    add css style section banner

---
+++ src/components/main/componentsMain/BtnOrigin/BtnOrigin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './BtnOrigin.css'
+function BtnOrigin() {
+    return (
+        <>
+                <button className='bg-orange-600 py-2.5 md:py-3 px-6 md:px-8 rounded-[15px] font-bold text-xl text-zinc-50 cursor-pointer btn_after'>
+                    <a href="#">Explore Products</a>
+                </button>
+        </>
+    )
+}
+
+export default BtnOrigin
--- src/components/main/sections/section1/Banner.jsx
+++ src/components/main/sections/section1/Banner.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
-
+import './Banner.css'
+import BtnOrigin from '../../componentsMain/BtnOrigin/BtnOrigin'
 function Banner() {
     return (
         <>
             <section>
                 <div className="container">
                     <div>
-                        <h5>YOUR</h5>
-                        <h2>ONE STOP SHOP</h2>
-                        <p>E-Bazar is your one-stop shop for all your grocery needs. We have
+                        <h5 className='text-3xl sm:text-[35px] md:text-[42px] xl:text-[47px] font-Lato-Bold font-bold tracking-[-0.03rem] text-zinc-800 mb-2'>YOUR</h5>
+                        <h2 className='text-[45px] sm:text-[50px] md:text-[65px] xl:text-[80px] font-Lato-Bold font-bold tracking-[-0.04rem] text-zinc-800 mb-4'>ONE STOP SHOP</h2>
+                        <p className='tracking-[0.03rem] mb-5 relative p_after max-w-150'>E-Bazar is your one-stop shop for all your grocery needs. We have
                             a wide selection of fresh produce, meats, seafood, dairy, and
                             baked goods, all at affordable prices. Plus, we offer free delivery.
                         </p>
-                        <div>
-                            <button>
-                                <a href="#">Explore Products</a>
-                            </button>
+                        <div className='mt-12 text-right max-w-290'>
+                            <BtnOrigin/>
                         </div>
                     </div>
                 </div>
