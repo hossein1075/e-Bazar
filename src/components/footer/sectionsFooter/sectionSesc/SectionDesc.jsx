f3d64d2b48cc89917aacb3e7efccb2c4fabd88c2
Hossein <hosseinh1075@gmail.com>
2025-10-21 10:12:32 +0330

    add css style section first footer

--- src/components/footer/sectionsFooter/sectionSesc/SectionDesc.jsx
+++ src/components/footer/sectionsFooter/sectionSesc/SectionDesc.jsx
@@ -5,45 +5,57 @@ import InfoBtnOrigin from '../../../main/componentsMain/BtnOrigin/InfoBtnOrigin'
 function SectionDesc() {
     return (
         <>
-            <section>
-                <div className="container">
-                    <div>
+            <section className=''>
+                <div className="container flex items-center justify-center">
+                    <div className='flex flex-wrap justify-center gap-8'>
                         <div>
-                            <img src="/icons/logo.png" alt="logo" />
-                            <p>
+                            <img src="/icons/logo.png" className='mb-8' alt="logo" />
+                            <p className='tracking-tightest4 max-w-54'>
                                 Welcome to Cobbler, your go-to destination for exquisite footwear & exceptional craftsmanship.
                                 With a passion for quality and timeless style,
                                 we pride ourselves with this.
                             </p>
                         </div>
                         <div>
-                            <h5>Categories</h5>
+                            <h5 className='text-27px font-bold font-Lato-Bold mt-4 md:mt-0 mb-4 tracking-tightest3'>Categories</h5>
                             <ul>
-                                <li>
+                                <li className='font-RobotoSerif-Regular tracking-tightest4 mb-2 hover:text-orange-600 transition-original'>
                                     <a href="#">Food & Drinks</a>
+                                </li>
+                                <li className='font-RobotoSerif-Regular tracking-tightest4 mb-2 hover:text-orange-600 transition-original'>
                                     <a href="#">Vegetables</a>
+                                </li>
+                                <li className='font-RobotoSerif-Regular tracking-tightest4 mb-2 hover:text-orange-600 transition-original'>
                                     <a href="#">Fruits</a>
+                                </li>
+                                <li className='font-RobotoSerif-Regular tracking-tightest4 mb-2 hover:text-orange-600 transition-original'>
                                     <a href="#">Dairy</a>
                                 </li>
                             </ul>
                         </div>
                         <div>
-                            <h5>Essential Links</h5>
+                            <h5 className='text-27px font-bold font-Lato-Bold mt-4 md:mt-0 mb-4 tracking-tightest3'>Essential Links</h5>
                             <ul>
-                                <li>
+                                <li className='font-RobotoSerif-Regular tracking-tightest4 mb-2 hover:text-orange-600 transition-original'>
                                     <a href="#">Home</a>
+                                </li>
+                                <li className='font-RobotoSerif-Regular tracking-tightest4 mb-2 hover:text-orange-600 transition-original'>
                                     <a href="#">Cart</a>
+                                </li>
+                                <li className='font-RobotoSerif-Regular tracking-tightest4 mb-2 hover:text-orange-600 transition-original'>
                                     <a href="#">Shop</a>
+                                </li>
+                                <li className='font-RobotoSerif-Regular tracking-tightest4 mb-2 hover:text-orange-600 transition-original'>
                                     <a href="#">Product Detail</a>
                                 </li>
                             </ul>
                         </div>
                         <div>
-                            <h5>Subscribe to Our Newsletter</h5>
-                            <div>
-                                <input type="text" placeholder='Your Email' />
+                            <h5 className='text-27px font-bold font-Lato-Bold mt-4 md:mt-0 mb-4 tracking-tightest3'>Subscribe to Our Newsletter</h5>
+                            <div className='mb-3.75'>
+                                <input type="text" placeholder='Your Email' className='outline-none border-b-1 border-solid border-neutral-600 w-full tracking-tightest4' />
                             </div>
-                            <BtnOrigin text={InfoBtnOrigin[5].info}/>
+                            <BtnOrigin text={InfoBtnOrigin[5].info} />
                         </div>
                     </div>
                 </div>
