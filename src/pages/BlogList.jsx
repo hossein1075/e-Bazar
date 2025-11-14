0860487fbb1559555c0c08b0930edece11eeec5e
Hossein <hosseinh1075@gmail.com>
2025-11-14 09:50:19 +0330

    add section first blog list page

--- src/pages/BlogList.jsx
+++ src/pages/BlogList.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../components/header/Header'
 import Footer from '../components/footer/Footer'
+import ShopBanner from '../components/Shop/Title/ShopBanner'
 function BlogList() {
   return (
     <>
     <Header/>
+    <ShopBanner
+    title='Blogs'
+    />
     <Footer/>
     </>
   )
