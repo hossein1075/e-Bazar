d252cefa36bd762e2891b1639350ffebf73a7c82
Hossein <hosseinh1075@gmail.com>
2025-11-14 11:20:10 +0330

    add html components BlogDetailComments in section 3 blog detail page

---
+++ src/BlogDetail/BlogDetail/BlogDetailComments/BlogDetailComments.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+function BlogDetailComments() {
+    return (
+        <>
+            <section>
+                <div className="container">
+                    <div>
+                        <div>
+                            <span>24 Aug, 2024</span>
+                            <span>/</span>
+                            <span> 02 Comments</span>
+                        </div>
+                        <div>
+                            <h4>Fun and Safe Outdoor Toys for Active Kids: Keep Them Engaged and Healthy</h4>
+                            <p>
+                                As parents, we all want our children to be active, healthy, and happy. In today's digital age,
+                                it's more important than ever to encourage outdoor play, where kids can explore, run, jump,
+                                and develop their physical and social skills. Outdoor toys are a fantastic way to keep kids engaged,
+                                entertained, and most importantly, safe while they enjoy the fresh air.
+                                In this blog, we'll explore some of the best outdoor toys that combine fun with safety,
+                                ensuring your little ones stay active and healthy.
+                            </p>
+                            <div>
+                                <div>svg</div>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur. Nullam in quis pharetra pellentesque faucibus.
+                                    Sit id rhoncus sodales ultrices mauris egestas mauris.
+                                    Aliquam in nulla rutrum erat consectetur est netus quam.
+                                </p>
+                                <h5>Emma Grace</h5>
+                            </div>
+                        </div>
+                        <h6>1. Ride-On Toys</h6>
+                        <p>Outdoor play is crucial for a child's overall development. It helps improve physical strength,
+                            coordination, and balance while also promoting mental well-being.
+                            Kids who play outside regularly are more likely to develop strong social skills and a love for nature. Additionally,
+                            outdoor play reduces screen time, helping to improve sleep patterns and reduce the risk of obesity.
+                            With the right toys, outdoor play can be both fun and safe.
+                        </p>
+                        <div>
+                            <div>
+                                <img src="/images/BlogDetail/blog-detail-2.png" alt="BlogDetailComments" />
+                            </div>
+                            <div>
+                                <img src="/images/BlogDetail/blog-detail-2.png" alt="BlogDetailComments" />
+                            </div>
+                        </div>
+                         <h6>2. A Complex Genius</h6>
+                        <p>Outdoor play is crucial for a child's overall development. It helps improve physical strength,
+                            coordination, and balance while also promoting mental well-being.
+                            Kids who play outside regularly are more likely to develop strong social skills and a love for nature. Additionally,
+                            outdoor play reduces screen time, helping to improve sleep patterns and reduce the risk of obesity.
+                            With the right toys, outdoor play can be both fun and safe.
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default BlogDetailComments
