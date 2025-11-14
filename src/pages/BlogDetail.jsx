c593bbc7c7923058d7432f2e4c6221a7846836d7
Hossein <hosseinh1075@gmail.com>
2025-11-14 11:01:20 +0330

    add header & footer blog detail page

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import Footer from '../components/footer/Footer'
+import Header from '../components/header/Header'
+import ShopBanner from '../components/Shop/Title/ShopBanner'
 function BlogDetail() {
   return (
-    <div>BlogDetail</div>
+    <>
+    <Header/>
+    <Footer/>
+    </>
   )
 }
 
