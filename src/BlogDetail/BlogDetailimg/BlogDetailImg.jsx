e3e3fe877ad007dd6dbba5ffb577a1fa9d215d7b
Hossein <hosseinh1075@gmail.com>
2025-11-14 11:09:02 +0330

    add section second blog detail page

---
+++ src/BlogDetail/BlogDetailimg/BlogDetailImg.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function BlogDetailImg() {
+  return (
+    <>
+    <section className='py-8 sm:py-12'>
+        <div className="container">
+            <div>
+                <img src="/images/BlogDetail/blog-detail-1.png" className='w-full rounded-10px' alt="BlogDetail" />
+            </div>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default BlogDetailImg
--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Footer from '../components/footer/Footer'
 import Header from '../components/header/Header'
 import ShopBanner from '../components/Shop/Title/ShopBanner'
+import BlogDetailImg from '../BlogDetail/BlogDetailimg/BlogDetailImg'
 function BlogDetail() {
   return (
     <>
     <Header/>
-    <ShopBanner/>
+    <ShopBanner 
+    title='Blog Detail'
+    />
+    <BlogDetailImg/>
     <Footer/>
     </>
   )
